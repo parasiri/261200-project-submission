0cd9be2860bb84999c332f3f149872812e96b874
parasiri <119008921+parasiri@users.noreply.github.com>
2023-10-17 14:11:36 +0700

    Update StackedBucket.java

--- core/src/com/mymaenak/game/StackedBucket.java
+++ core/src/com/mymaenak/game/StackedBucket.java
@@ -1,14 +1,1 @@
-package com.mymaenak.game;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Rectangle;
-
-public class StackedBucket {
-    public Rectangle rectangle;
-    public Texture texture;
-
-    public StackedBucket(Rectangle rectangle, Texture texture) {
-        this.rectangle = rectangle;
-        this.texture = texture;
-    }
-}
